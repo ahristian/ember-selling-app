6a0f1c3bf7b35a7f3fd0b2e6c8bf1d63a2a8e9d0
ahristian <hristian.andrei@gmail.com>
2020-12-15 00:16:35 -0700

    adding products

--- app/models/product.js
+++ app/models/product.js
@@ -1,7 +1,14 @@
 import DS from 'ember-data';
+import { computed, set } from '@ember/object';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   image: DS.attr('string'),
+  price: DS.attr('number'),
+  displayPrice: computed('price', function(){
+    let price = parseFloat(this.get('price')),
+    tax = price * 25/100;
+    return price + tax;
+  })
   
 });
