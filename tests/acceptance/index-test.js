46fa89d11c80fe95256216fe913207d502336778
ahristian <hristian.andrei@gmail.com>
2020-12-16 11:41:20 -0700

    adding tests

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,25 +1,25 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click, server } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-module('Acceptance | index', function (hooks) {
+module('Acceptance | index', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('visiting /', async function (assert) {
+  test('should visiting /', async function(assert) {
     this.server.createList('product', 10);
     await visit('/');
 
-   // assert.equal(currentURL(), '/');
-    assert.equal(this.element.querySelectorAll('.card').length, 10);
+    assert.equal(currentURL(), '/');
+    assert.equal(this.element.querySelectorAll('a.card').length, 10);
   });
-});
-/* 
-test('clicking the card', async function (assert) {
-  this.server.create('product', { name: "My awesome product" });
+
+ test( 'clicking the card', async function(assert){
+  this.server.create('product', {name: "My awesome product"});
   await visit('/');
-  await click('a.card:first');
-  assert.equal(find('h2').text(), "My awesome product");
-}) */
+  await click('a.card');
+  assert.equal(this.element.querySelectorAll('h2')[0].textContent, "My awesome product");
+});
+});
 
