fa79682ea7a447cd9410ba7ae8fa15f25a98865f
ahristian <hristian.andrei@gmail.com>
2020-12-17 23:59:52 -0700

    adding child parent relation

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -3,11 +3,11 @@ import { visit, currentURL, click, server } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-module('Acceptance | index', function(hooks) {
+module('Acceptance | index', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('should visiting /', async function(assert) {
+  test('should visiting /', async function (assert) {
     this.server.createList('product', 10);
     await visit('/');
 
@@ -15,11 +15,11 @@ module('Acceptance | index', function(hooks) {
     assert.equal(this.element.querySelectorAll('a.card').length, 10);
   });
 
- test( 'clicking the card', async function(assert){
-  this.server.create('product', {name: "My awesome product"});
-  await visit('/');
-  await click('a.card');
-  assert.equal(this.element.querySelectorAll('h2')[0].textContent, "My awesome product");
-});
+  test('clicking the card', async function (assert) {
+    this.server.create('product', { name: "My awesome product" });
+    await visit('/');
+    await click('a.card');
+    assert.equal(this.element.querySelectorAll('h2')[0].textContent, "My awesome product");
+  });
 });
 
