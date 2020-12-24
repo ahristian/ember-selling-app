ccf92062a515269435993c9f47972caf05b05cbe
ahristian <hristian.andrei@gmail.com>
2020-12-23 21:18:09 -0700

    adding polymorphic model

--- app/models/category.js
+++ app/models/category.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
+import Imageable from './imageable'
 
-  export default DS.Model.extend({
+  export default Imageable.extend({
     name: DS.attr('string'),
-    image: DS.attr('string'),
     products: DS.hasMany('product'),
     children: DS.hasMany('category', {inverse: 'parent'}),
     parent: DS.belongsTo('category', {inverse: 'children'})
