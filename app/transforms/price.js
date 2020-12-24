9a7a50ee966d821334e88375ee7c0664bca91e4e
ahristian <hristian.andrei@gmail.com>
2020-12-23 21:21:57 -0700

    transforms

---
+++ app/transforms/price.js
@@ -0,0 +1,11 @@
+import Transform from '@ember-data/serializer/transform';
+
+export default class PriceTransform extends Transform {
+  deserialize(serialized) {
+    return parseFloat(serialized).toFixed(2);
+  }
+
+  serialize(deserialized) {
+    return parseFloat;
+  }
+}
