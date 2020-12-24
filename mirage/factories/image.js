5d94b9d8565482198b964afecadd618c25050839
ahristian <hristian.andrei@gmail.com>
2020-12-23 21:20:54 -0700

    transforms

--- mirage/factories/image.js
+++ mirage/factories/image.js
@@ -1,4 +1,7 @@
 import { Factory } from 'ember-cli-mirage';
+import faker from 'faker';
 
 export default Factory.extend({
+    name() { return faker.commerce.productName(); },
+    
 });
--- mirage/factories/product.js
+++ mirage/factories/product.js
@@ -1,7 +1,25 @@
-import { Factory } from 'ember-cli-mirage';
+import { Factory, association } from 'ember-cli-mirage';
 import faker from 'faker';
 
 export default Factory.extend({
-    name() { return faker.commerce.productName(); },
-    image() { return faker.random.image(250, 250); }
+    name() {
+        return faker.commerce.productName();
+    },
+    price(){
+        return faker.commerce.price();
+    },
+    traits() {
+        return [faker.commerce.productAdjective(),
+        faker.commerce.productAdjective(),
+        faker.commerce.productAdjective(),
+        faker.commerce.productAdjective(),
+        faker.commerce.productAdjective(), 
+        faker.commerce.productAdjective(), 
+        faker.commerce.productAdjective()];
+    },
+    image: association(),
+    afterCreate(product) {
+        product.createImage({ url: faker.image.image(250, 250) });
+        product.save();
+    }
 });
