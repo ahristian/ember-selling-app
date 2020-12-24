57026359c4fde59c94cc6904e4040f25f82cc167
ahristian <hristian.andrei@gmail.com>
2020-12-23 21:21:13 -0700

    transforms

--- app/models/product.js
+++ app/models/product.js
@@ -4,7 +4,8 @@ import Imageable from './imageable'
 
 export default Imageable.extend({
   name: DS.attr('string'),
-  price: DS.attr('number'),
+  price: DS.attr('price'),
+  traits: DS.attr('array'),
   displayPrice: computed('price', function(){
     let price = parseFloat(this.get('price')),
     tax = price * 25/100;
