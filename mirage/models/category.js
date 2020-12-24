fce2c1148b14f1700eecbac14196b7550da50733
ahristian <hristian.andrei@gmail.com>
2020-12-23 21:19:13 -0700

    adding polymorphic model

--- mirage/models/category.js
+++
@@ -1,6 +0,0 @@
-import { Model, hasMany, belongsTo } from 'ember-cli-mirage';
-
-export default Model.extend({
-    children: hasMany('category', {inverse: 'parent'}),
-    parent: belongsTo('category', {inverse: 'children'})
-});
